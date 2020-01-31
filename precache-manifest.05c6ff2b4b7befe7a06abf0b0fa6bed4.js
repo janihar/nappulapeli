12d2b7928b7c5610ee0fb0236b8e7dda359acb8d
Jani Harjula <harjula.jani@outlook.com>
2020-02-01 01:39:45 +0200

    Updates

---
+++ precache-manifest.05c6ff2b4b7befe7a06abf0b0fa6bed4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef3c6f81a4af7c975935da6a4eceadeb",
+    "url": "/janihar/nappulapeli/index.html"
+  },
+  {
+    "revision": "0105b68e4b41db69ae39",
+    "url": "/janihar/nappulapeli/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d9c365bac35712b51f98",
+    "url": "/janihar/nappulapeli/static/css/main.aaf36127.chunk.css"
+  },
+  {
+    "revision": "0105b68e4b41db69ae39",
+    "url": "/janihar/nappulapeli/static/js/2.4334e0c7.chunk.js"
+  },
+  {
+    "revision": "138a6d51240ab070e8d1b20c54be6394",
+    "url": "/janihar/nappulapeli/static/js/2.4334e0c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d9c365bac35712b51f98",
+    "url": "/janihar/nappulapeli/static/js/main.3c1fe238.chunk.js"
+  },
+  {
+    "revision": "5cf37b06672086d0b1dc",
+    "url": "/janihar/nappulapeli/static/js/runtime-main.71880a4d.js"
+  },
+  {
+    "revision": "12b65193cbc04c30df84fc0dad04ecca",
+    "url": "/janihar/nappulapeli/static/media/Koodarijahti_tunnus.12b65193.svg"
+  },
+  {
+    "revision": "694bf80960fdaa41adea9a7849fec66a",
+    "url": "/janihar/nappulapeli/static/media/koodarijahti_tausta_tre.694bf809.jpg"
+  },
+  {
+    "revision": "3fb72115eaf6c1d7f68e904933936f3d",
+    "url": "/janihar/nappulapeli/static/media/vincit_logo_red.3fb72115.jpg"
+  }
+]);
